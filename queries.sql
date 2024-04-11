50b07179801c056234f45e2a91b49680d800646c
Cerpint4xt <rcatagrundy@gmail.com>
2024-04-11 15:55:05 -0400

    change partition and clustering add create table for articles data

---
+++ queries.sql
@@ -0,0 +1,50 @@
+-- After Google Cloud Storage ingestion, you need to create a bucket to reference the queries for author. 
+
+-- Create External Table from Google Cloud Storage
+CREATE OR REPLACE EXTERNAL TABLE `all-news-project.all_news_data.external_table_all_news_data`
+OPTIONS (
+    FORMAT='PARQUET',
+    URIS=['gs://all-news-bucket-project/all_news_data/*']
+)
+
+
+-- Create Materialized Table from the external Table
+
+CREATE OR REPLACE TABLE `all-news-project.all_news_data.materialized_table_all_news_data`
+AS (
+    SELECT * FROM `all-news-project.all_news_data.external_table_all_news_data`
+)
+
+
+-- Create Partitioning `date` and clustering `publication`
+CREATE OR REPLACE TABLE `all-news-project-419219.all_news_data.all_news_partitioned_table`
+PARTITION BY date
+CLUSTER BY publication as (
+    SELECT * FROM `all-news-project-419219.all_news_data.materialized_table_all_news_data` 
+)
+
+
+
+
+-- After Google Cloud Storage ingestion, you need to create a bucket to reference the queries for articles. 
+
+Create External Table from Google Cloud Storage
+CREATE OR REPLACE EXTERNAL TABLE `all-news-project-419219.all_news_data.external_table_all_news_data_articles`
+OPTIONS (
+    FORMAT='PARQUET',
+    URIS=['gs://all-news-bucket-project/all_news_data_articles/*']
+)
+
+-- Create Materialized Table form the external Table
+
+CREATE OR REPLACE TABLE `all-news-project-419219.all_news_data.materialized_table_all_news_data_articles`
+AS (
+    SELECT * FROM `all-news-project-419219.all_news_data.external_table_all_news_data_articles`
+)
+
+-- Create Partitioning `date` and clustering `publication`
+CREATE OR REPLACE TABLE `all-news-project-419219.all_news_data.all_news_partitioned_table_articles`
+PARTITION BY date
+CLUSTER BY publication as (
+    SELECT * FROM `all-news-project-419219.all_news_data.materialized_table_all_news_data_articles` 
+)
