30621332c7055e48a45de5a5674b52f382a8c7c3
Cerpint4xt <rcatagrundy@gmail.com>
2024-04-15 16:26:24 -0400

    Update queries to create tables in GBQ

--- queries.sql
+++ queries.sql
@@ -1,26 +1,28 @@
+-- This queries should be run in BigQuery interface to create the necessary sources for dbt
+
 -- After Google Cloud Storage ingestion, you need to create a bucket to reference the queries for author. 
 
 -- Create External Table from Google Cloud Storage
-CREATE OR REPLACE EXTERNAL TABLE `all-news-project.all_news_data.external_table_all_news_data`
+CREATE OR REPLACE EXTERNAL TABLE `all-news-project.all_news_data.external_table_all_news_data_author`
 OPTIONS (
     FORMAT='PARQUET',
-    URIS=['gs://all-news-bucket-project/all_news_data/*']
+    URIS=['gs://all-news-bucket-project/all_news_data_author/*']
 )
 
 
 -- Create Materialized Table from the external Table
 
-CREATE OR REPLACE TABLE `all-news-project.all_news_data.materialized_table_all_news_data`
+CREATE OR REPLACE TABLE `all-news-project.all_news_data.materialized_table_all_news_data_author`
 AS (
-    SELECT * FROM `all-news-project.all_news_data.external_table_all_news_data`
+    SELECT * FROM `all-news-project.all_news_data.external_table_all_news_data_author`
 )
 
 
 -- Create Partitioning `date` and clustering `publication`
-CREATE OR REPLACE TABLE `all-news-project-419219.all_news_data.all_news_partitioned_table`
+CREATE OR REPLACE TABLE `all-news-project-419219.all_news_data.all_news_partitioned_table_author`
 PARTITION BY date
 CLUSTER BY publication as (
-    SELECT * FROM `all-news-project-419219.all_news_data.materialized_table_all_news_data` 
+    SELECT * FROM `all-news-project-419219.all_news_data.materialized_table_all_news_data_author` 
 )
 
 
