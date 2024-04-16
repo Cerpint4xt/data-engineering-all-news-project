f3c1e50da0245e9dfe4a1a8d739514acc09e0e80
Cerpint4xt <rcatagrundy@gmail.com>
2024-04-16 14:18:28 -0400

    update names and details

--- queries.sql
+++ queries.sql
@@ -3,26 +3,26 @@
 -- After Google Cloud Storage ingestion, you need to create a bucket to reference the queries for author. 
 
 -- Create External Table from Google Cloud Storage
-CREATE OR REPLACE EXTERNAL TABLE `all-news-project.all_news_data.external_table_all_news_data_author`
+CREATE OR REPLACE EXTERNAL TABLE `all-news-project-<project_id>.all_news_data.external_table_all_news_data_authors`
 OPTIONS (
     FORMAT='PARQUET',
-    URIS=['gs://all-news-bucket-project/all_news_data_author/*']
+    URIS=['gs://all-news-bucket-project/all_news_data_authors/*']
 )
 
 
 -- Create Materialized Table from the external Table
 
-CREATE OR REPLACE TABLE `all-news-project.all_news_data.materialized_table_all_news_data_author`
+CREATE OR REPLACE TABLE `all-news-project-<project-id>.all_news_data.materialized_table_all_news_data_authors`
 AS (
-    SELECT * FROM `all-news-project.all_news_data.external_table_all_news_data_author`
+    SELECT * FROM `all-news-project-<project-id>.all_news_data.external_table_all_news_data_authors`
 )
 
 
 -- Create Partitioning `date` and clustering `publication`
-CREATE OR REPLACE TABLE `all-news-project-419219.all_news_data.all_news_partitioned_table_author`
+CREATE OR REPLACE TABLE `all-news-project-<project-id>.all_news_data.all_news_partitioned_table_authors`
 PARTITION BY date
 CLUSTER BY publication as (
-    SELECT * FROM `all-news-project-419219.all_news_data.materialized_table_all_news_data_author` 
+    SELECT * FROM `all-news-project-<project-id>.all_news_data.materialized_table_all_news_data_authors` 
 )
 
 
@@ -30,8 +30,8 @@ CLUSTER BY publication as (
 
 -- After Google Cloud Storage ingestion, you need to create a bucket to reference the queries for articles. 
 
-Create External Table from Google Cloud Storage
-CREATE OR REPLACE EXTERNAL TABLE `all-news-project-419219.all_news_data.external_table_all_news_data_articles`
+-- Create External Table from Google Cloud Storage
+CREATE OR REPLACE EXTERNAL TABLE `all-news-project-<project-id>.all_news_data.external_table_all_news_data_articles`
 OPTIONS (
     FORMAT='PARQUET',
     URIS=['gs://all-news-bucket-project/all_news_data_articles/*']
@@ -39,14 +39,14 @@ OPTIONS (
 
 -- Create Materialized Table form the external Table
 
-CREATE OR REPLACE TABLE `all-news-project-419219.all_news_data.materialized_table_all_news_data_articles`
+CREATE OR REPLACE TABLE `all-news-project-<project-id>.all_news_data.materialized_table_all_news_data_articles`
 AS (
-    SELECT * FROM `all-news-project-419219.all_news_data.external_table_all_news_data_articles`
+    SELECT * FROM `all-news-project-<project-id>.all_news_data.external_table_all_news_data_articles`
 )
 
 -- Create Partitioning `date` and clustering `publication`
-CREATE OR REPLACE TABLE `all-news-project-419219.all_news_data.all_news_partitioned_table_articles`
+CREATE OR REPLACE TABLE `all-news-project-<project-id>.all_news_data.all_news_partitioned_table_articles`
 PARTITION BY date
 CLUSTER BY publication as (
-    SELECT * FROM `all-news-project-419219.all_news_data.materialized_table_all_news_data_articles` 
+    SELECT * FROM `all-news-project-<project-id>.all_news_data.materialized_table_all_news_data_articles` 
 )
